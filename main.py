e177a63a278c5a7696963d9b625ab96af62d8251
Elias Greil <eliasgreil@Eliass-Air.home>
2024-10-30 19:05:42 +0100

    parameter müssen noch angepasst werden. aber ist Besser

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ curve.plot() """
 import matplotlib.pyplot as plt
 import numpy as np
 from simsopt._core import load
-from simsopt.geo import plot, MeanSquaredCurvature
+from simsopt.geo import plot, MeanSquaredCurvature, CurveSurfaceDistance, CurveCurveDistance
 from simsopt.geo import SurfaceRZFourier, create_equally_spaced_curves, \
     CurveLength, curves_to_vtk
 from simsopt.field import Current, coils_via_symmetries, BiotSavart
@@ -18,12 +18,14 @@ from scipy.optimize import minimize
 # replace "NAME_OF_FILE_YOU_DOWNLOADED" with the name you gave the file
 res = load(f'serial0021326.json')
 
-ncoils = 4
+ncoils = 3
 
 MAXITER = 300
 
-nphi = 64
-ntheta = 64
+USE_PRESIM = False
+
+nphi = 32
+ntheta = 32
 s = SurfaceRZFourier.from_vmec_input('input', range="full torus", nphi=nphi, ntheta=ntheta)
 
 coils = res[1]
@@ -35,7 +37,9 @@ print(np.size(coils))
 
 base_curves = create_equally_spaced_curves(ncoils, s.nfp, stellsym=True, R0=1, R1=0.6, order=5)
 base_currents = [Current(1.0) * 1e5 for i in range(ncoils)]
-#coils = coils_via_symmetries(base_curves, base_currents, s.nfp, True)
+base_currents[0].fix_all()
+
+coils = coils_via_symmetries(base_curves, base_currents, s.nfp, True)
 
 ##calculating magnetic field from coils:
 
@@ -47,19 +51,28 @@ bs.set_points(s.gamma().reshape((-1, 3)))
 Jf = SquaredFlux(s, bs)
 Jls = [CurveLength(c) for c in base_curves]
 
-LENGTH_WEIGHT=0
-LENGTH_TARGET = 18.0
-C_WEIGHT = 1
-MSC_THRESHOLD = 10 #1e-6
+LENGTH_WEIGHT = 0.01
+C_WEIGHT = 0.05
+DIST_WEIGHT = 2
+CCD_WEIGHT = 1
+
+LENGTH_TARGET = 10
+MSC_THRESHOLD = 9
+DIST_THRESHOLD = 0.1
+CCDIST_TH = 0.3
 
 Jl = QuadraticPenalty(sum(Jls), LENGTH_TARGET, "max")
 Jmscs = [MeanSquaredCurvature(c) for c in base_curves]
-Jc = sum(QuadraticPenalty(J, MSC_THRESHOLD) for J in Jmscs)
+Jc = sum(QuadraticPenalty(J, MSC_THRESHOLD, 'max') for J in Jmscs)
+
+Jd = CurveSurfaceDistance(base_curves, s, DIST_THRESHOLD)
+
+Jccd = CurveCurveDistance(base_curves, CCDIST_TH)
 
 ## penalty function: (change penalties fitting to your problem)
 ## (here it's length of coils curves. use radius for 1st problem i guess)
 
-JF = Jf + LENGTH_WEIGHT * Jl + C_WEIGHT * Jc
+JF = 2*Jf + LENGTH_WEIGHT * Jl + C_WEIGHT * Jc + DIST_WEIGHT * Jd + CCD_WEIGHT * Jccd
 
 
 B_dot_n = np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)
@@ -67,7 +80,7 @@ B_dot_n = np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)
 print('Initial max|B dot n|:', np.max(np.abs(B_dot_n)))
 
 
-plot(coils + [s], engine="plotly", close=True)
+#plot(coils + [s], engine="plotly", close=True)
 
 def fun(dofs):
     JF.x = dofs
