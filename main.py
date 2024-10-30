0cea26a734e9fa092259a9e8e8921c3c4d2401d8
Elias Greil <eliasgreil@EDUPERS232054.UNI-GRAZ.AT>
2024-10-30 10:24:47 +0100

    second commit, example downloaded

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ curve.set_dofs([0,0,0,0,1,0,0,0,1])
 curve.plot() """
 
 import matplotlib.pyplot as plt
+import numpy as np
 from simsopt._core import load
 # replace "NAME_OF_FILE_YOU_DOWNLOADED" with the name you gave the file
 res = load(f'serial0021326.json')
@@ -13,7 +14,8 @@ res = load(f'serial0021326.json')
 coils = res[1]
 surface = res[0]
 
+print(np.size(coils))
 
 from simsopt.geo import plot
-plot(coils + surface, engine="plotly", close=True)
+plot(coils + [surface[1]], engine="plotly", close=True)
 
--- readme.txt
+++ readme.txt
@@ -5,3 +5,12 @@ Elias Greil, Sebastian Riepl,
 
 Simsopt
 aufsetzen, Beispiele: Magnetfeld visualisieren
+
+
+https://quasr.flatironinstitute.org/model/0021326
+
+https://github.com/itpplasma/ALPES/tree/main
+
+https://github.com/hiddenSymmetries/simsopt
+
+https://simsopt.readthedocs.io/en/latest/overview.html
